56b029ab58a03e705d12b9ab9dcfe9fd59149c47
Ervin Natareno <ean1207@gmail.com>
2023-01-05 17:27:11 -0800

    Added show/hide password functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// Quote Carousel
 let slideIndex = 1;
 runSlideShow(slideIndex); // runSlideShow(1)
 
@@ -36,3 +37,17 @@ function runSlideShow(n) {
 }
 
 setInterval(runSlideShow, 7000);
+
+// Show Password Function
+const showPassword = document.querySelector("#show-password");
+const passwordField = document.querySelector("#password");
+
+showPassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    showPassword.setAttribute("name", "eye-off");
+  } else {
+    showPassword.setAttribute("name", "eye");
+  }
+  const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordField.setAttribute('type', type)
+});
