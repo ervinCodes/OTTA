7273d9a05d32fd69e27c1b32e66dc719894b12e5
Ervin Natareno <ean1207@gmail.com>
2023-01-23 18:04:40 -0800

    Carousel Changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,55 @@
-// Quote Carousel
-let slideIndex = 1;
-runSlideShow(slideIndex); // runSlideShow(1)
+const slider = document.querySelector(".slider");
+const carousel = document.querySelector(".carousel");
 
-// function currentSlide(n) {
-//   runSlideShow((slideIndex = n));
-// }
+const leftArrow = document.querySelector(".previous");
+const rightArrow = document.querySelector(".next");
+console.log(rightArrow);
 
-function runSlideShow(n) {
-  let slides = document.querySelectorAll(".slides");
-  let quotes = document.querySelectorAll(".quotes");
-  // console.log(dots);
+let direction;
 
-  // if slideIndex > 3, reset slideIndex = 1
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  // if slideIndex < 3
-  if (slideIndex < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.add("hidden");
-    slides[slideIndex - 1].classList.remove("hidden");
-  }
-
-  slideIndex++;
+function startCarousel() {
+  setInterval(function () {
+    direction = -1;
+    carousel.style.justifyContent = "flex-start";
+    // slider.style.transform = "translate(-33%)";
+  }, 5000);
 }
 
-setInterval(runSlideShow, 7000);
+startCarousel();
+
+leftArrow.addEventListener("click", function () {
+  if (direction === -1) {
+    slider.appendChild(slider.firstElementChild);
+    direction = 1;
+  }
+
+  carousel.style.justifyContent = "flex-end";
+  slider.style.transform = "translate(33%)";
+});
+
+function goRight() {
+  direction = -1;
+  carousel.style.justifyContent = "flex-start";
+  slider.style.transform = "translate(-33%)";
+}
+
+rightArrow.addEventListener("click", goRight);
+
+slider.addEventListener("transitionend", function () {
+  if (direction === -1) {
+    slider.appendChild(slider.firstElementChild);
+  } else if (direction === 1) {
+    slider.prepend(slider.lastElementChild);
+  }
+
+  slider.style.transition = "none";
+  slider.style.transform = "translate(0px)";
+  setTimeout(function () {
+    slider.style.transition = "all 0.5s";
+  });
+});
+
+setInterval(goRight, 10000);
 
 // Show Password Function
 const showPassword = document.querySelector("#show-password");
@@ -39,6 +61,7 @@ showPassword.addEventListener("click", function () {
   } else {
     showPassword.setAttribute("name", "eye");
   }
-  const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordField.setAttribute('type', type)
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
 });
