742d6ec8172ff14215f520627b443207c7335c5c
Ervin Natareno <ean1207@gmail.com>
2023-02-03 17:16:11 -0800

    Navbar responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        "davys-grey": "#4A4E59",
         "eerie-black": "#181517",
         "silver": "#C1BFBE",
-        "silver-metallic": "#A9ACB0",
-        "metallic-sunburst": "#A38041",
-        "jasmine": "#F0D574",
         "sonic-silver": "#737373",
         "vegas-gold": "#CDAD5C",
-        "satin-sheen": "#C69D42",
-        "dark-goldenrd": "#B58632",
-        "queen-blue": "#5C6D8B",
         "space-cadet": "#1C2646"
       },
       spacing: {
